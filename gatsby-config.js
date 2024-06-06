abad6ec968a073cabed471283f280bef6d34b45f
Simon-Bertram <simonbertram@hotmail.com>
2024-06-06 18:22:22 +0100

    Finished Gatsby tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
+    title: `This is going to be great`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [`gatsby-plugin-image`, `gatsby-plugin-sharp`],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
